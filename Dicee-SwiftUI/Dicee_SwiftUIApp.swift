69d0ddef1bc65128626637877fa008691dded0e3
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-03-14 09:54:14 -0400

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Aiperim Akisheva on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
